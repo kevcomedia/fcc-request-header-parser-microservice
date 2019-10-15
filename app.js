75276f18e88c3f84ba3934c7fa67202553dc271b
Kev Comedia <kevcomedia@gmail.com>
2019-10-15 17:30:45 +0800

    Implement api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('Hello world')
+app.get('/api/whoami', (request, response) => {
+  const json = {
+    ipaddress: request.ip,
+    language: request.get('Accept-Language'),
+    software: request.get('User-Agent'),
+  }
+  response.json(json)
 })
 
 module.exports = app

8f8499a1ea392a8f9a73cde2ac2f186c7f631edd
Kev Comedia <kevcomedia@gmail.com>
2019-10-15 17:50:24 +0800

    Add home page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'pug')
+
+app.use('/assets', express.static(__dirname + '/public'))
+
+app.get('/', (request, response) => {
+  const { protocol, hostname } = request
+  response.render('index', { host: `${protocol}://${hostname}` })
+})
+
 app.get('/api/whoami', (request, response) => {
   const json = {
     ipaddress: request.ip,
